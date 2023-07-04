741dbf436c708a55441ab0b743fa646475260eec
Emir <emirsurmen@gmail.com>
2023-07-04 22:05:39 +0300

    Change image paths

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,9 +7,9 @@ const Home: Component = () => {
         <div class={styles.centered}>
             <p style="padding: 30px">Şehirli, herkesten önce parmağının ucundan tüm olup biteni görmeni hedefliyor. İnsanların paylaştıkları olayları anında haritada görebilecek, kendin gördüğün olayları da paylaşabileceksin. Şehirli, sadece bir sosyal medya değil; <underline>bağımsız</underline> bir haber aracı.
             </p>
-            <a class={styles.a}><img src="/src/assets/appstore_download.png" alt="App Store'dan indir" height="70px" style="vertical-align: center"/></a>
+            <a class={styles.a}><img src="https://raw.githubusercontent.com/Sehirli/sehirli-website/main/src/assets/appstore_download.png" alt="App Store'dan indir" height="70px" style="vertical-align: center"/></a>
             <a>&ensp;</a>
-            <a class={styles.a}><img alt="Google Play'den alın" src="/src/assets/googleplay_download.png" height="93px" style="vertical-align: bottom"/></a>
+            <a class={styles.a}><img alt="Google Play'den alın" src="https://raw.githubusercontent.com/Sehirli/sehirli-website/main/src/assets/googleplay_download.png" height="93px" style="vertical-align: bottom"/></a>
         </div>
     );
 }
